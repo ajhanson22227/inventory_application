05de23d2f8f42c6b0e242ff1bdd0e6fbb8e3256d
ajhanson22227 <ajhanson22227@gmail.com>
2020-10-30 09:13:19 -0500

    Create ItemInstance model

--- models/item.js
+++ models/item.js
@@ -6,9 +6,9 @@ const ItemSchema = new Schema(
   {
     name: {type: String, required: true},
     description: {type: String, required: true},
-    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
-    price: {type: Number, required: true},
-    stock: {type: Number, required: true}
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true}
+    // price: {type: Number, required: true},
+    // stock: {type: Number, required: true}
   }
 );
 
